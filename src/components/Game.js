335080391c8f19b697d53d4e3959cbe124debe20
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-05-01 22:39:39 -0400

    Code list rendering and manipulation

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,43 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, {useState} from 'react';
 import '../style/game.css';
+import uniqid from 'uniqid';
 
-function Game(props) {
+function Game({props}) {
+    const [list, setList] = useState(props);
+    
+    function shuffleList() {
+        const clone = structuredClone(list);
+        for (let i = clone.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [clone[i], clone[j]] = [clone[j], clone[i]];
+        }
+        setList(clone);
+    }
 
-
+    function resetToggles() {
+        const clone = structuredClone(list);
+        clone.forEach(item => {
+            item.toggle = false;
+        });
+        setList(clone);
+    }
+    
+    function renderList() {
+        const array = [];
+        list.forEach(item => {
+            const element = <div key={uniqid()} className='card'>
+                <div className='picture'></div>
+                <div className='name'>{item.name}</div>
+            </div>
+            array.push(element);
+        });
+        return array;
+    }
+    
     return (
-        <main>
+        <main onClick={shuffleList}>
+            {renderList()}
         </main>
     )
 }
--- src/components/Initialize.js
+++ src/components/Initialize.js
@@ -4,12 +4,17 @@ import Game from './Game';
 
 const obamna = {name: 'obamna', src: '', toggle: false};
 const juan = {name: 'juan', src: '', toggle: false};
+const hampter = {name: 'hampter', src: '', toggle: false};
+const slug = {name: 'slug', src: '', toggle: false};
+const giga = {name: 'giga', src: '', toggle: false};
+const sickos = {name: 'sickos', src: '', toggle: false};
+const terry = {name: 'terry', src: '', toggle: false};
+const vegeta = {name: 'vegeta', src: '', toggle: false};
+const ted = {name: 'ted', src: '', toggle: false};
 
-const array = [obamna, juan];
+const array = [obamna, juan, hampter, slug, giga, sickos, terry, vegeta, ted];
 
 function Initialize() {
-
-
     return (
         <Game props={array} />
     )
