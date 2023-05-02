90792deb383c07da3ca9bd397ab110cb07f3f342
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-05-01 20:34:07 -0400

    Create basic structure and boilerplates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import './style/app.css';
+import Game from './components/Game';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <div>Meme Memory</div>
+        <div id="score"></div>
+        <div>Made by: antwonthedamaja</div>
+      </header>
+      <Game />
     </div>
   );
 }
---
+++ src/components/Game.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/game.css';
+
+function Game() {
+
+    return (
+        <main>
+            
+        </main>
+    )
+}
+
+export default Game;
